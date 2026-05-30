c27424f136fcf2dcdea002896f13e0216d90832d
Alex Berg <arberg@gmail.com>
2026-05-30 16:42:28 +0200

    tmp

--- build.sh
+++ build.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
-set +ex
+set -e
 
-. env
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-# docker build -f Dockerfile-Alpine -t $USERNAME/$IMAGE:latest-alpine .
+# shellcheck source=env
+. "$SCRIPT_DIR/env"
 
-docker build -f Dockerfile-Ubuntu -t $USERNAME/$IMAGE:latest .
+DOCKERFILE="Dockerfile-Ubuntu"
+IMAGE_NAME="$USERNAME/$IMAGE"
+BUILD_CONTEXT_DIR="$(pwd -P)"
+BUILD_DATE="$(date -Iseconds)"
+BUILD_HOST="$(hostname)"
 
+build_image() {
+    docker build \
+        --build-arg BUILD_CONTEXT_DIR="$BUILD_CONTEXT_DIR" \
+        --build-arg BUILD_DATE="$BUILD_DATE" \
+        --build-arg BUILD_HOST="$BUILD_HOST" \
+        "$@" \
+        -f "$DOCKERFILE" \
+        .
+}
 
-SVN_VERSION_ECHO=$(docker run --rm $USERNAME/$IMAGE:latest svn --version | head -1)
+build_image -t "$IMAGE_NAME:latest"
+
+SVN_VERSION_ECHO="$(docker run --rm "$IMAGE_NAME:latest" svn --version | head -1)"
 echo "$SVN_VERSION_ECHO"
-SVN_VERSION_ECHO=$(echo "$SVN_VERSION_ECHO" | cut -d" " -f3)
-echo $SVN_VERSION_ECHO > VERSION
-echo "Built svn version '$SVN_VERSION_ECHO'"
 
+SVN_VERSION="$(echo "$SVN_VERSION_ECHO" | cut -d" " -f3)"
+echo "$SVN_VERSION" > VERSION
+echo "Built svn version '$SVN_VERSION'"
+
+build_image \
+    --build-arg SVN_VERSION="$SVN_VERSION" \
+    -t "$IMAGE_NAME:latest" \
+    -t "$IMAGE_NAME:$SVN_VERSION"

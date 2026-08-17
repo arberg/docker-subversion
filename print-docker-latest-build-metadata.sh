55d1133786c712339a89211cc873f06ace5ec265
Alex Berg <arberg@gmail.com>
2026-08-17 08:33:27 +0200

    print

--- print-docker-latest-build-metadata.sh
+++ print-docker-latest-build-metadata.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 # shellcheck source=env
 . "$SCRIPT_DIR/env"
 
-docker image inspect "$USERNAME/$IMAGE:latest"
+docker image inspect "$USERNAME/$IMAGE:latest" | jq $@
